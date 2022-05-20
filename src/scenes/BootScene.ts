dedbcf060b822a7ca12e84a4045808c34ca78159
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2022-05-20 17:58:30 +0700

    adding animations, sprites, and update map

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -2,25 +2,27 @@ export class BootScene extends Phaser.Scene {
     private map!: Phaser.Tilemaps.Tilemap;
     private tileset!: Phaser.Tilemaps.Tileset;
     private sky!: Phaser.Tilemaps.TilemapLayer;
-    private cloud!: Phaser.Tilemaps.TilemapLayer;
     private control!: Phaser.Cameras.Controls.FixedKeyControl;
 
     preload(): void {
-        this.load.image('tiles','./assets/tiles/tiles.png');
-        this.load.tilemapTiledJSON('superMario','./assets/maps/superMario.json');
+        this.load.pack('preload', './assets/pack.json', 'preload');
     }
 
     create(): void {
         this.map = this.make.tilemap({key:"superMario"});
         this.tileset = this.map.addTilesetImage('tiles','tiles');
         this.sky = this.map.createLayer('sky',this.tileset,0,0);
-        this.cloud = this.map.createLayer('cloud',this.tileset);
-        this.map.createLayer('grass',this.tileset);
-        this.map.createLayer('pipe',this.tileset);
         this.map.createLayer('foreground',this.tileset);
-        this.map.createLayer('castle',this.tileset);
-        this.map.createLayer('stairs',this.tileset);
+        this.map.createLayer('decoration',this.tileset);
+        this.map.createLayer('objectOnGround',this.tileset);
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+        const objects = this.map.getObjectLayer('objects').objects as any[];
+        objects.forEach((object) => {
+            if (object.type === 'mario') {
+                let mario = this.add.sprite(object.x,object.y,'mario');
+                mario.setOrigin(0,0);
+            }
+        });
         var cursors = this.input.keyboard.createCursorKeys();
         var controlConfig = {
             camera: this.cameras.main,
